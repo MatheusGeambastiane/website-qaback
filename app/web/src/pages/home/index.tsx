95ca3bfdb33e49e1f86f504a4782a2fe195d87d3
Ryan Oliveira Brito <97217108+ryanoliveiragit@users.noreply.github.com>
2023-07-23 01:02:57 -0300

    add img and ajust components

--- app/web/src/pages/home/index.tsx
+++ app/web/src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 import { Login } from "../../components/login";
 import { Container } from "./styles";
+import Image from '../../../public/loginBanner.png';
 
 export const Home = () => {
   return (
     <Container>
+        <img src={Image} alt="LoginImage"/>
       <Login />
     </Container>
   );
