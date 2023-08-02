49cea707593913ff9bc97712879d5d413a6a2075
mgeambastiane <mgeambastiane@gmail.com>
2023-08-01 22:47:10 -0300

    Feature: removendo o back para raiz e fazer deploy

--- app/web/src/hooks/useFilter.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { ContextProducts, ProductsType } from "../context/products";
-
-export const useFilter = (name: keyof ProductsType, type: string) => {
-  const { products } = useContext(ContextProducts);
-  const filteredItems = products.filter((item: ProductsType) => item[name] === type);
-  return filteredItems;
-}
